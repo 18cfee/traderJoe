18828914a49d589dbc15b28ddd90468725405e76
Carl Fee <cfee@arbfund.com>
2018-11-23 18:20:45 -0700

    .

--- CorrelationCalculator/PullAllFilesIntoDataStructure.py
+++ CorrelationCalculator/PullAllFilesIntoDataStructure.py
@@ -1,29 +1,58 @@
 import os,glob
+import csv
 folder_path = "C:\\Users\\cfee\\git\\trading\\tickerData"
-count = 0
+count = 1
 dict = {}
+set = set()
+fieldnames = []
+fieldnames.append("Date")
 for filename in glob.glob(os.path.join(folder_path, 'USA*.txt')):
   with open(filename, 'r') as f:
     print(count)
     text = f.read()
     print (filename)
-    print (len(text))
+    #print (len(text))
     lines = text.splitlines()
     for line in lines:
         val = line.split(',')
         date = val[0]
         if(date != "DATE" and int(date) > 20010000):
-            print(date)
+            # print(date)
             value = val[1]
             if date in dict:
                 list = dict.get(date)
                 list[count] = value
             else:
+                set.add(date)
                 list = [None] * 54
-                list[count] = value
-                dict[date] = list
+                list2 = [date]
+                list2.extend(list)
+                list2[count] = value
+                print("the length is " ,len(list2))
+                dict[date] = list2
     count = count + 1
+    fieldnames.append(fieldnames)
 
-for key in dict.keys():
-    print(key)
-    print(dict[key])
+# for key in dict.keys():
+#     print(key)
+#     print(dict[key])
+
+listK = sorted(set)
+print(listK)
+
+arrayS = []
+
+for item in listK:
+    arrayS.append(dict[item])
+
+with open('data.csv', 'w') as csvfile:
+    a = range(55)
+    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for item in listK:
+        print(item)
+        writer.writerow(dict[item])
+
+
+
+
